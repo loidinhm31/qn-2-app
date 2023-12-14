950185fd7ec442e7811cdeab9019c986c1d4aad5
loidinhm31 <loidinh.git@gmail.com>
2023-12-14 20:06:43 +0700

    Add creation session modal

--- src/app/pages/management-session/management-session.page.spec.ts
+++ src/app/pages/management-session/management-session.page.spec.ts
@@ -1,5 +1,5 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ManagementSessionPage } from './management-session.page';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ManagementSessionPage } from "./management-session.page";
 
 describe('ManagementSessionPage', () => {
   let component: ManagementSessionPage;
